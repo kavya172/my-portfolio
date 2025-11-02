e625de071f1ac6363d114c204c0d402ea57aefb8
Kavya Srinivasa <kavyasrinivasa@Kavyas-MacBook-Air.local>
2025-11-02 18:04:57 +0530

    Project Description changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,20 +12,20 @@ function Projects() {
       />
       <ProjectCard
         title="GxCapture"
-        description="GxCapture-TechRefresh creates standardized processes and a platform that can be used to execute current and future lifecycle management program for any technologies."
+        description="GxCapture is an intent management tool that captures data and runs it through automated workflows for financial processing or summarized outcomes."
         skills={["Java", "Spring Boot", "JavaScript", "React", "MS-SQL", "Nginx"]}
       />
     </div>
       <div className="projects-container">
         <ProjectCard
         title="GxWorkflow"
-        description="GxCapture-TechRefresh creates standardized processes and a platform that can be used to execute current and future lifecycle management program for any technologies."
-        skills={["Java", "Spring Boot", "JavaScript", "React", "MS-SQL", "Nginx"]}
+        description="GxWorkflow is a workflow automation tool inspired by BPMN, enabling custom workflows using SendTask, UserTask, ScriptTask, and ServiceTask to streamline process execution and integrate system and user actions."
+        skills={["Java", "Spring Boot", "JavaScript", "React", "MS-SQL", "PostgresSQL", "Git"]}
       />
       <ProjectCard
         title="GxClaims"
-        description="GxCapture-TechRefresh creates standardized processes and a platform that can be used to execute current and future lifecycle management program for any technologies."
-        skills={["Java", "Spring Boot", "JavaScript", "React", "MS-SQL", "Nginx"]}
+        description="GxClaims is a validation tool that performs automated claim-to-claim comparison between legacy and modernized systems, ensuring business rules and financial outcomes match during migration."
+        skills={["Java", "Spring Boot", "JavaScript", "React", "MS-SQL", "PostgresSQL"]}
       />
     </div>
     <div className="projects-container"></div>
